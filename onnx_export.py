92929806c96f56448a6458157473d15328157357
xzy-git <78792221+xzy-git@users.noreply.github.com>
2023-01-18 19:39:49 +0800

    Add files via upload

--- onnx_export.py
+++ onnx_export.py
@@ -10,13 +10,11 @@ from model_onnx import SynthesizerTrn
 import utils
 from hubert import hubert_model_onnx
 
-def main(HubertExport,NetExport,Netname):
-
-    path = Netname
+def main(HubertExport,NetExport,Netpath,Netfile):
 
     if(HubertExport):
         device = torch.device("cuda")
-        hubert_soft = hubert_model_onnx.hubert_soft("hubert/model.pt")
+        hubert_soft = hubert_model_onnx.hubert_soft("hubert/hubert-soft-0d54a1f4.pt")
         test_input = torch.rand(1, 1, 16000)
         input_names = ["source"]
         output_names = ["embed"]
@@ -34,12 +32,12 @@ def main(HubertExport,NetExport,Netname):
                         output_names=output_names)
     if(NetExport):
         device = torch.device("cuda")
-        hps = utils.get_hparams_from_file(f"checkpoints/{path}/config.json")
+        hps = utils.get_hparams_from_file(f"{Netpath}/config.json")
         SVCVITS = SynthesizerTrn(
             hps.data.filter_length // 2 + 1,
             hps.train.segment_size // hps.data.hop_length,
             **hps.model)
-        _ = utils.load_checkpoint(f"checkpoints/{path}/model.pth", SVCVITS, None)
+        _ = utils.load_checkpoint(f"{Netpath}/{Netfile}", SVCVITS, None)
         _ = SVCVITS.eval().to(device)
         for i in SVCVITS.parameters():
             i.requires_grad = False
@@ -57,7 +55,7 @@ def main(HubertExport,NetExport,Netname):
                             test_pitch.to(device),
                             test_sid.to(device)
                         ),
-                        f"checkpoints/{path}/model.onnx",
+                        f"{Netpath}/model.onnx",
                         dynamic_axes={
                             "hidden_unit": [0, 1],
                             "pitch": [1]
@@ -71,13 +69,31 @@ def main(HubertExport,NetExport,Netname):
 
 if __name__ == '__main__':
     parser=argparse.ArgumentParser()
-    parser.add_argument('--export_hubert',type=int,help='Export hubert or not, 0 for not export, 1 for export')
-    parser.add_argument('--export_model',type=int,help='Export the model or not, 0 for not export, 1 for export')
-    parser.add_argument('--model_name',type=str,help='The name of the model. It helps to find the path')
+    parser.add_argument('--export_hubert',type=str,help='Export hubert or not')
+    parser.add_argument('--export_model',type=str,help='Export the model or not')
+    parser.add_argument('--model_path',type=str,help='The path of the model')
+    parser.add_argument('--model_file',type=str,help='The filename of the model')
 
     args = parser.parse_args()
-    if not args.export_hubert or not args.export_model or not args.model_name:
+    if not args.export_hubert or not args.export_model or not args.model_path or not args.model_file:
         parser.print_help()
         sys.exit(0)
     else:
-        main(args.export_hubert,args.export_model,args.model_name)
+        print(args.export_hubert)
+        print(args.export_model)
+        print(args.model_path)
+        print(args.model_file)
+
+        export_hubert=True
+        if(args.export_hubert=='True'):
+            export_hubert=True
+        elif(args.export_hubert=='False'):
+            export_hubert=False
+
+        export_model=True
+        if(args.export_model=='True'):
+            export_model=True
+        elif(args.export_model=='False'):
+            export_model=False
+
+        main(export_hubert,export_model,args.model_path,args.model_file)
