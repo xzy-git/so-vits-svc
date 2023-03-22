b9297f6763754384816c46cf799d739bfbc26f6c
xzy-git <78792221+xzy-git@users.noreply.github.com>
2023-03-22 17:51:05 +0800

    Add files via upload

--- onnx_export.py
+++ onnx_export.py
@@ -1,3 +1,6 @@
+import argparse
+import os
+import json
 import torch
 from torchaudio.models.wav2vec2.utils import import_fairseq_model
 from fairseq import checkpoint_utils
@@ -16,9 +19,7 @@ def get_hubert_model():
     return model
 
 
-def main(HubertExport, NetExport):
-    path = "SoVits4.0"
-
+def main(NetExport,Netpath,Netfile,Projectname):
     '''if HubertExport:
         device = torch.device("cpu")
         vec_path = "hubert/checkpoint_best_legacy_500.pt"
@@ -46,14 +47,15 @@ def main(HubertExport, NetExport):
                                   },
                           }
                           )'''
+
     if NetExport:
         device = torch.device("cpu")
-        hps = utils.get_hparams_from_file(f"checkpoints/{path}/config.json")
+        hps = utils.get_hparams_from_file(f"{Netpath}/config.json")
         SVCVITS = SynthesizerTrn(
             hps.data.filter_length // 2 + 1,
             hps.train.segment_size // hps.data.hop_length,
             **hps.model)
-        _ = utils.load_checkpoint(f"checkpoints/{path}/model.pth", SVCVITS, None)
+        _ = utils.load_checkpoint(f"{Netpath}/{Netfile}.pth", SVCVITS, None)
         _ = SVCVITS.eval().to(device)
         for i in SVCVITS.parameters():
             i.requires_grad = False
@@ -66,6 +68,11 @@ def main(HubertExport, NetExport):
         input_names = ["c", "f0", "mel2ph", "uv", "noise", "sid"]
         output_names = ["audio", ]
         SVCVITS.eval()
+
+        onnx_path=f"{Netpath}/{Projectname}-{Netfile}"
+        if not os.path.exists(onnx_path):
+            os.makedirs(onnx_path)
+
         torch.onnx.export(SVCVITS,
                           (
                               test_hidden_unit.to(device),
@@ -75,7 +82,7 @@ def main(HubertExport, NetExport):
                               test_noise.to(device),
                               test_sid.to(device)
                           ),
-                          f"checkpoints/{path}/model.onnx",
+                          f"{onnx_path}/{Projectname}-{Netfile}_SoVits.onnx",
                           dynamic_axes={
                               "c": [0, 1],
                               "f0": [1],
@@ -89,6 +96,56 @@ def main(HubertExport, NetExport):
                           input_names=input_names,
                           output_names=output_names)
 
+        with open(f"{Netpath}/config.json", 'r') as f:
+            train_config=json.load(f)
+
+        Rate=train_config['data']['sampling_rate']
+        Hop=train_config['data']['hop_length']
+        Characters=list(train_config['spk'].keys())
+
+        infer_config=\
+        {\
+           "Folder" : "NyaruTaffySo",\
+            "Name" : "NyaruTaffy-SoVits",\
+            "Type" : "SoVits",\
+            "Rate" : 44100,\
+            "Hop" : 512,\
+            "Cleaner" : "",\
+            "Hubert": "hubert4.0",\
+            "SoVits4": True,\
+            "Characters" : ["Taffy","Nyaru"]\
+        }
+
+        infer_config['Folder']=f"{Projectname}-{Netfile}"
+        infer_config['Name']=f"{Projectname}-{Netfile}"
+        infer_config['Rate']=Rate
+        infer_config['Hop']=Hop
+        infer_config['Characters']=Characters
+
+        with open(f"{Netpath}/{Projectname}-{Netfile}.json", 'w') as f:
+            json.dump(infer_config,f)
 
 if __name__ == '__main__':
-    main(False, True)
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--export_model',type=str,help='Export the model or not')
+    parser.add_argument('--model_path',type=str,help='The path of the model')
+    parser.add_argument('--model_file',type=str,help='The filename of the model')
+    parser.add_argument('--project_name',type=str,help='Project name, affect the name of the model folder, model file and config file')
+
+    args = parser.parse_args()
+    if not args.export_model or not args.model_path or not args.model_file or not args.project_name:
+        parser.print_help()
+        sys.exit(0)
+    else:
+        print(args.export_model)
+        print(args.model_path)
+        print(args.model_file)
+        print(args.project_name)
+
+        export_model=True
+        if(args.export_model=='True'):
+            export_model=True
+        elif(args.export_model=='False'):
+            export_model=False
+
+        main(export_model,args.model_path,args.model_file,args.project_name)
